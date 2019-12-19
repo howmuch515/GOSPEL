ae49b28c63be43f6f29a9002371d7524f4fd02a5
howmuch515 <another.face0515@gmail.com>
2019-12-19 11:06:24 +0900

    :recycle: make abstract_run_trace, make_opcode function.

--- RunTraceGenerator.py
+++ RunTraceGenerator.py
@@ -8,37 +8,34 @@ OPCODE_PATTERN = re.compile(r"CommandInvocation\((\S+)\)")
 
 
 # abstract run_trace
-def abstractRunTrace(event_log_file_name):
+def abstract_run_trace(event_log_file_name):
     run_trace_list = []
     with evtx.Evtx(event_log_file_name) as log:
-        record_length = 0
         for record in log.records():
-            record_length += 1
             xml = record.xml()
             m = re.search(OPCODE_PATTERN, xml)
             if m is None:
-                print(f"[!] Not match!: {xml}")
+                print(f"[-] Not match!")
             else:
                 opcode = m.group(1)
-                print(f"[*] opcode: {opcode}")
+                print(f"[+] opcode: {opcode}")
                 run_trace_list.append([{"key": "command_let", "value": opcode}])
-        print(f"[+] record_length = {record_length}")
     return run_trace_list
 
 
 # make opcode graph from run_trace
-def makeOpcode(run_trace):
-    # make opcode graph DataFrame.
+def make_opcode(run_trace):
     opcode_graph = {}
     cmdlet_list = []
-    with open("cmdlet.json", "r") as j:
-        cmdlet_list = json.load(j)
-        ## make opcode_graph with pandas and numpy.
+
+    # make opcode_graph with pandas.
+    with open("cmdlet_list.json", "r") as f:
+        cmdlet_list = json.load(f)
         opcode_graph = pd.DataFrame(0, index=cmdlet_list, columns=cmdlet_list)
 
     next_opcode = ""
-    for h, i in enumerate(run_trace):
-        for j in i:
+    for i, v1 in enumerate(run_trace):
+        for j in v1:
             if j["key"] == "command_let":
                 current_opcode = j["value"]
 
@@ -46,29 +43,28 @@ def makeOpcode(run_trace):
                 if current_opcode not in cmdlet_list:
                     print(f"[-] {current_opcode} is unknown!")
                     continue
-                
+
                 pre_opcode = next_opcode
                 next_opcode = current_opcode
 
                 # skip the first opcode
-                if h == 0:
+                if i == 0:
                     continue
+                else:
+                    print(f"[+] {pre_opcode} ==> {next_opcode}")
+                    opcode_graph[pre_opcode][next_opcode] += 1
 
-                print(f"[+] {pre_opcode} ==> {next_opcode}")
-                opcode_graph[pre_opcode][next_opcode] += 1
     return opcode_graph
 
 
 def main():
     event_log_file_name = sys.argv[1]
     output_json_file_name = event_log_file_name.split(".")[0] + ".json"
-    print(f"[+] INPUT_FILE_NAME = {event_log_file_name}")
-
-    # abstract run_trace from event log
-    run_trace_list = abstractRunTrace(event_log_file_name)
+    print(f"[+] INPUT_FILE_NAME = {event_log_file_name}") # abstract run_trace from event log
+    run_trace_list = abstract_run_trace(event_log_file_name)
 
     # make opcode graph from run_trace
-    opcode_graph = makeOpcode(run_trace_list)
+    opcode_graph = make_opcode(run_trace_list)
 
     # output result to file
     with open(output_json_file_name, 'w') as j:
