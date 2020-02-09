0b6a9f3465687586d5f266ec25e9b80c4f2e3710
howmuch515 <another.face0515@gmail.com>
2020-02-10 00:51:39 +0900

    :recycle: refactoring plot_score.

--- scripts/plot_score.py
+++ scripts/plot_score.py
@@ -3,7 +3,10 @@ import pandas as pd
 import numpy as np
 from sys import argv
 
-def main():
+DISPERSION_NUM = 0.06
+
+
+def plot_score():
     if len(argv) > 2:
         COMPARE_SCORE_FILE = argv[1]
         OBFUSCATED_SCORE_FILE = argv[2]
@@ -11,21 +14,21 @@ def main():
         COMPARE_SCORE_FILE = "../test1_result.csv"
         OBFUSCATED_SCORE_FILE = "../test02_output.csv"
 
+    # init matplotlib
     fig, ax = plt.subplots()
-    ax.set_ylabel("RMS_SCORE")
 
+    # set test1 result
     df = pd.read_csv(COMPARE_SCORE_FILE)
     diff_y = []
     for y, v in enumerate(df.values.tolist()[:]):
         for x, vv in enumerate(v):
             if type(vv) == float:
                 diff_y.append(vv)
-                print(vv)
 
+    diff_x = np.random.normal(0, DISPERSION_NUM, size=len(diff_y))
+    ax.plot(diff_x, diff_y, "o", color="C1", label="Different scripts")
 
-    diff_x = np.random.normal(0, 0.06, size=len(diff_y))
-    diff_dot = ax.plot(diff_x, diff_y, "o", color="C1", label="Different scripts")
-
+    # set test2 result
     df = pd.read_csv(OBFUSCATED_SCORE_FILE)
     obfuscated_y = []
     for i, v in enumerate(df.values.tolist()[:]):
@@ -35,19 +38,21 @@ def main():
 
             if isinstance(vv, float):
                 obfuscated_y.append(vv*0.0001)
-    obfuscated_y_size = len(obfuscated_y)
-    print(f"[+] obfuscated_y_size: {obfuscated_y_size}")
 
-    obfuscated_x = np.random.normal(0, 0.06, size=len(obfuscated_y))
-    obfuscated_dot = ax.plot(obfuscated_x, obfuscated_y, "o", color="C2", label="Obfuscated scripts")
+    obfuscated_x = np.random.normal(0, DISPERSION_NUM, size=len(obfuscated_y))
+    ax.plot(obfuscated_x, obfuscated_y, "o", color="C2", label="Obfuscated scripts")
 
+    # matplotlib settings
+    ax.set_ylabel("RMS_SCORE")
     ax.legend()
     ax.set_xlim(-1, 1)
     plt.tick_params(labelbottom=False)
     plt.show()
 
-    # save figure
-    # plt.savefig("score_plot.png")
+
+def main():
+    plot_score()
+
 
 if __name__ == '__main__':
     main()
